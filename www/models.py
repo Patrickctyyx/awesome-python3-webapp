5ac83273e227bf72d66d358782eba6f45e368704
Patrickctyyx <873948000@qq.com>
2016-09-28 12:22:20 +0800

    Update models.py
    9.28
      接触过Flask之后这一切就比较熟悉了。

--- www/models.py
+++ www/models.py
@@ -11,10 +11,12 @@ import time, uuid
 
 from orm import Model, StringField, BooleanField, FloatField, TextField
 
-def next_id():
+# MVC中的Model
+
+def next_id():  # 生成不重复的id
     return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
 
-class User(Model):
+class User(Model):  # 存放用户信息
     __table__ = 'users'
 
     id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
@@ -25,25 +27,25 @@ class User(Model):
     image = StringField(ddl='varchar(500)')
     created_at = FloatField(default=time.time)
 
-class Blog(Model):
+class Blog(Model):  # 存放博客
     __table__ = 'blogs'
 
     id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
     user_id = StringField(ddl='varchar(50)')
     user_name = StringField(ddl='varchar(50)')
-    user_image = StringField(ddl='varchar(500)')
+    user_image = StringField(ddl='varchar(500)')  # 上面三条和user对应
     name = StringField(ddl='varchar(50)')
     summary = StringField(ddl='varchar(200)')
     content = TextField()
     created_at = FloatField(default=time.time)
 
-class Comment(Model):
+class Comment(Model):  # 存放评论
     __table__ = 'comments'
 
     id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
-    blog_id = StringField(ddl='varchar(50)')
+    blog_id = StringField(ddl='varchar(50)')  # 和blog对应
     user_id = StringField(ddl='varchar(50)')
     user_name = StringField(ddl='varchar(50)')
-    user_image = StringField(ddl='varchar(500)')
+    user_image = StringField(ddl='varchar(500)')  # 上面三条和user对应
     content = TextField()
     created_at = FloatField(default=time.time)
