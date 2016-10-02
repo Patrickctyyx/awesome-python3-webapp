d9169656af9b7b878aa98a9371b6e0f9752099e6
Patrickctyyx <873948000@qq.com>
2016-10-02 23:35:57 +0800

    Update coroweb.py
    10.2 Daily Learn:
        1.说是daily，结果好久没看了...今天公交车上理解了一些
        2.现在web框架差不多理解到位了
        3.但是像平面里面那些简化之类的还是不能

--- www/coroweb.py
+++ www/coroweb.py
@@ -93,7 +93,16 @@ def has_request_arg(fn):
             raise ValueError('request parameter must be the last named parameter in function: %s%s' % (fn.__name__, str(sig)))
     return found
 
-# 这以下还需要继续理解，下面的评论很棒！仔细研究研究！
+# 这个RQ是用来封装http处理函数的
+# 1.接收并分析url函数所需要的参数
+# 2.分析http method, content type并做出相应处理
+# 3.把参数封装到kw中作为self._func(**kw)参数
+# 另外关于kw的参数
+# request.match_info的参数： match_info主要是保存像@get('/blog/{id}')里面的id，就是路由路径里的参数
+# GET的参数： 像例如/?page=2
+# POST的参数： api的json或者是网页中from
+# request参数： 有时需要验证用户信息就需要获取request里面的数据
+# 详情见day5下面的评论
 class RequestHandler(object):
 
     def __init__(self, app, fn):
@@ -106,6 +115,7 @@ class RequestHandler(object):
         self._required_kw_args = get_required_kw_args(fn)
 
     # __call__()方法是为了让他的实例为函数，比如RequestHandler(app, fn)就相当于调用了函数
+    # request是前端发过来的
     async def __call__(self, request):
         kw = None
         if self._has_var_kw_arg or self._has_named_kw_args or self._required_kw_args:
@@ -113,6 +123,7 @@ class RequestHandler(object):
                 if not request.content_type:
                     return web.HTTPBadRequest('Missing Content-Type.')
                 ct = request.content_type.lower()
+                # startswith() 方法用于检查字符串是否是以指定子字符串开头
                 if ct.startswith('application/json'):
                     params = await request.json()
                     if not isinstance(params, dict):
@@ -130,6 +141,7 @@ class RequestHandler(object):
                     for k, v in parse.parse_qs(qs, True).items():
                         kw[k] = v[0]
         if kw is None:
+            # match_info主要是保存像@get('/blog/{id}')里面的id，就是路由路径里的参数
             kw = dict(**request.match_info)
         else:
             if not self._has_var_kw_arg and self._named_kw_args:
@@ -158,32 +170,45 @@ class RequestHandler(object):
         except APIError as e:
             return dict(error=e.error, data=e.data, message=e.message)
 
+# 注册静态文件
 def add_static(app):
+    # os.path.abspath返回绝对路径
+    # os.path.dirname返回文件的目录的路径
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+    # 告诉python静态文件储存的路径
     app.router.add_static('/static/', path)
     logging.info('add static %s => %s' % ('/static/', path))
 
+# 注册url函数
 def add_route(app, fn):
     method = getattr(fn, '__method__', None)
     path = getattr(fn, '__route__', None)
     if path is None or method is None:
         raise ValueError('@get or @post not defined in %s.' % str(fn))
+    # 把函数编程协程函数
     if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
         fn = asyncio.coroutine(fn)
     logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__, ', '.join(inspect.signature(fn).parameters.keys())))
     app.router.add_route(method, path, RequestHandler(app, fn))
 
+# 一次性注册一个py文件里所有的url函数
 def add_routes(app, module_name):
+    # rfind() 返回字符串最后一次出现的位置，如果没有匹配项则返回-1
+    # 比如handlers
     n = module_name.rfind('.')
     if n == (-1):
         mod = __import__(module_name, globals(), locals())
     else:
-        name = module_name[n+1:]
+        name = module_name[n+1:]  # name是点后面的
+        # 相当于from module_name[:n] import name
         mod = getattr(__import__(module_name[:n], globals(), locals(), [name]), name)
+    # dir(mod)就是这个模块里面的所有属性
     for attr in dir(mod):
+        # 排除系统属性
         if attr.startswith('_'):
             continue
         fn = getattr(mod, attr)
+        # 如果可以调用
         if callable(fn):
             method = getattr(fn, '__method__', None)
             path = getattr(fn, '__route__', None)
