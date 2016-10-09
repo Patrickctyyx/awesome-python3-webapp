6f2ed6143dba0ad1c88b808d94ea0ed560a53206
Patrickctyyx <873948000@qq.com>
2016-10-09 21:32:28 +0800

    Update config.py
    10.9 Daily Learn:
        好久没继续了
        今天的比较简单，明天开始就又要回归正常的进度啦!
        加油！

--- www/config.py
+++ www/config.py
@@ -30,15 +30,21 @@ class Dict(dict):
 def merge(defaults, override):
     r = {}
     for k, v in defaults.items():
+        # 如果重载内容中又相同的key
         if k in override:
+            # 如果对应的值还是一个dict，就要再检查里面的内容是不是完全相同
+            # 于是就重复上面的，也就是递归
             if isinstance(v, dict):
                 r[k] = merge(v, override[k])
+            # 不是的话就直接覆盖
             else:
                 r[k] = override[k]
+        # 另外的就还是按照默认的来
         else:
             r[k] = v
     return r
 
+# 把dict变成可以有点运算的特殊dict
 def toDict(d):
     D = Dict()
     for k, v in d.items():
