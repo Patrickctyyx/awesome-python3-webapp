2449e2e905ce9f0c754d623bc8541ab850ea4fa6
Patrickctyyx <873948000@qq.com>
2016-10-19 19:11:01 +0800

    Update handlers.py
    10.19 Daily Learn:
        这么多天终于把这一天的代码看完了，挺有收获的，不得不佩服廖大大的安全处理！

--- www/handlers.py
+++ www/handlers.py
@@ -20,6 +20,7 @@ from config import configs
 COOKIE_NAME = 'awesession'
 _COOKIE_KEY = configs.session.secret
 
+# 根据用户信息建立一个超级复杂的cookie
 def user2cookie(user, max_age):
     '''
     Generate cookie str by user.
@@ -28,8 +29,10 @@ def user2cookie(user, max_age):
     expires = str(int(time.time() + max_age))
     s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
     L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
+    # join后产生的是一个字符串
     return '-'.join(L)
 
+# 解析那个超级复杂的cookie
 @asyncio.coroutine
 def cookie2user(cookie_str):
     '''
@@ -38,19 +41,25 @@ def cookie2user(cookie_str):
     if not cookie_str:
         return None
     try:
+        # split后又变成了一个list
         L = cookie_str.split('-')
+        # cookie_str是由三部分组成
         if len(L) != 3:
             return None
         uid, expires, sha1 = L
+        # 时间不能超过规定的时间expires
         if int(expires) < time.time():
             return None
+        # 查找用户是不是伪造的
         user = yield from User.find(uid)
         if user is None:
             return None
         s = '%s-%s-%s-%s' % (uid, user.passwd, expires, _COOKIE_KEY)
+        # 看看自己加密的和cookie中的是不是相同
         if sha1 != hashlib.sha1(s.encode('utf-8')).hexdigest():
             logging.info('invalid sha1')
             return None
+        # 密码可不能随便就让人得到了，即使已经加密
         user.passwd = '******'
         return user
     except Exception as e:
@@ -82,6 +91,7 @@ def signin():
         '__template__': 'signin.html'
     }
 
+# login
 @post('/api/authenticate')
 def authenticate(*, email, passwd):
     if not email:
@@ -91,50 +101,63 @@ def authenticate(*, email, passwd):
     users = yield from User.findAll('email=?', [email])
     if len(users) == 0:
         raise APIValueError('email', 'Email not exist.')
+    # 数据库查询得到的结果是多个，这里相当于Flask 中的.first
     user = users[0]
     # check passwd:
     sha1 = hashlib.sha1()
     sha1.update(user.id.encode('utf-8'))
     sha1.update(b':')
     sha1.update(passwd.encode('utf-8'))
+    # 最后结果应该是id:password组合再加密
     if user.passwd != sha1.hexdigest():
         raise APIValueError('passwd', 'Invalid password.')
     # authenticate ok, set cookie:
     r = web.Response()
+    # 把cookie和response一起返回
     r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
     user.passwd = '******'
     r.content_type = 'application/json'
+    # 因为这里要把body json化，密码当然是不能让人知道的，所以上面密码就要处理
     r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
     return r
 
 @get('/signout')
 def signout(request):
     referer = request.headers.get('Referer')
+    # 先找到当前用户的cookie等信息
     r = web.HTTPFound(referer or '/')
+    # 把cookie也就是登录状态删除掉
     r.set_cookie(COOKIE_NAME, '-deleted-', max_age=0, httponly=True)
     logging.info('user signed out.')
     return r
 
+# 开头是一个数字字母以上，后面接@再接一个以上的数字字母符号，最后是.还有一到四个数字字母符号结尾
 _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
 _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
 
 @post('/api/users')
 def api_register_user(*, email, name, passwd):
+    # .strip()用来移除空格，当然括号里面是什么就移除什么
+    # 也就是说姓名不能为空格
     if not name or not name.strip():
         raise APIValueError('name')
+    # 邮箱的格式必须对
     if not email or not _RE_EMAIL.match(email):
         raise APIValueError('email')
     if not passwd or not _RE_SHA1.match(passwd):
         raise APIValueError('passwd')
+    # 账号不能重复
     users = yield from User.findAll('email=?', [email])
     if len(users) > 0:
         raise APIError('register:failed', 'email', 'Email is already in use.')
     uid = next_id()
+    # 其实前端过来的密码已经被加密过一次了，这里再加密一次
     sha1_passwd = '%s:%s' % (uid, passwd)
     user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(), image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
     yield from user.save()
     # make session cookie:
     r = web.Response()
+    # 这样注册完就不用再登陆一次了
     r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
     user.passwd = '******'
     r.content_type = 'application/json'
